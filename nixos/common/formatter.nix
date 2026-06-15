ea0efb11c2d960d66c046c297d036f3ba7cc3db8
SHNZHN-01 <shnzhn@protonmail.com>
2026-06-15 18:01:54 -0300

    changes

--- nixos/common/formatter.nix
+++ nixos/common/formatter.nix
@@ -1,19 +1,20 @@
 { inputs, ... }: {
-  flake.nixosModules.formatter = { ... }: {
-    imports = [ inputs.treefmt-nix.flakeModule ];
-
-    treefmt = {
-      programs = {
-        nixfmt.enable = true;
-        stylua.enable = true;
-        prettier.enable = true;
-        ruff-format.enable = true;
-        gofumpt.enable = true;
-        rustfmt.enable = true;
-        shfmt.enable = true;
-        clang-format.enable = true;
-        taplo.enable = true;
-      };
-    };
+  flake.nixosModules.formatter = { pkgs, ... }: {
+    environment.systemPackages = [
+      (inputs.treefmt-nix.lib.mkWrapper pkgs {
+        projectRootFile = ".git/config";
+        programs = {
+          nixfmt.enable = true; # .nix   (nixfmt-rfc-style)
+          stylua.enable = true; # .lua
+          prettier.enable = true; # .js .ts .json .css .html .yaml .md
+          ruff-format.enable = true; # .py
+          gofumpt.enable = true; # .go
+          rustfmt.enable = true; # .rs
+          shfmt.enable = true; # .sh
+          clang-format.enable = true; # .c .cpp .h .java
+          taplo.enable = true; # .toml
+        };
+      })
+    ];
   };
 }
